2778f4d700e95b103524a018bb6d0b502a76e9b8
leandrolannister <leandrohendrix@gmail.com>
2021-04-24 17:37:20 -0300

    String

---
+++ String/Aspas.js
@@ -0,0 +1,11 @@
+var heSaid = "Alan said, \"Peter is learning JS\".";
+console.log(heSaid);
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+console.log(myStr);
+
+let phrase = 'I think it is "easy" to solve';
+console.log(phrase);
+
+var myStr = '<a href="http://www.totvs.com.br" target="_blank">Link</a>';
+console.log(myStr);
