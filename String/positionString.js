0cbd8ae9a4bc582c91393ea3a798dcc1c1bb4489
leandrolannister <leandrohendrix@gmail.com>
2021-04-27 08:12:12 -0300

    const->var

---
+++ String/positionString.js
@@ -0,0 +1,10 @@
+const NAME = "Ribeiro";
+
+console.log('last letter is ->', NAME[NAME.length - 1]);
+
+console.log('second last letter is ->', NAME[NAME.length - 2]);
+
+console.log('third last letter is ->', NAME[NAME.length - 2]);
+
+
+
---
+++ Variaveis/const/const_1.js
@@ -0,0 +1,7 @@
+let s = [5, 6, 7];
+
+s = [1, 2, 3]; // An error will be print
+
+s[2] = 45; // js accept it
+
+console.log(s);
