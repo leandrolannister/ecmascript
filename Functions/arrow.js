8ba0e3fe786c913aa7bc4624ebf6a72c53a12f67
leandrolannister <leandrohendrix@gmail.com>
2021-04-30 07:49:04 -0300

    Arrow->Functions

---
+++ Functions/arrow.js
@@ -0,0 +1,13 @@
+const name = function(nome){
+    return nome;   
+ }
+ console.log(name('Leandro'));
+ 
+ const name_2 = (nome) => {
+     return nome;
+ }
+ console.log(name_2('Soares'));
+ 
+ const name_3 = nome => nome;
+ console.log(name_3('Ribeiro'));
+ 
