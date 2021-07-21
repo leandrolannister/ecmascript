e3334889c6f2d36d1ebe8923da2bd27a5b9369c1
leandrolannister <leandrohendrix@gmail.com>
2021-07-20 21:31:13 -0300

    RegExp Match

---
+++ RegExp/Match_3.js
@@ -0,0 +1,7 @@
+let quoteSample = "Leandro Soares";
+let vowelRegex = /[aeiou]/ig; 
+let result = quoteSample.match(vowelRegex);
+
+console.log(result);
+
+
---
+++ RegExp/Test_3.js
@@ -0,0 +1,6 @@
+let humStr = "I'll hum a song";
+let hugStr = "Bear hug";
+let huRegex = /hu./;
+
+console.log('First', huRegex.test(hugStr));
+console.log('Second', huRegex.test(hugStr));
