5085a06050664d46bd3fab133da152a5a3623011
leandrolannister <leandrohendrix@gmail.com>
2021-08-10 08:39:58 -0300

    Regexp

--- RegExp/group.js
+++ RegExp/group.js
@@ -1,11 +1,11 @@
 let catSrt = 'cat';
-let regExp = /[a-e]at/;
+let regExp = /[a-e]at/g;
 
 let batSrt = 'bat';
 let matSrt = 'mat';
 
 console.log(
-    'Cat =>', catSrt.match(regExp)[0],
-    'Bat =>', batSrt.match(regExp)[0],
+    'Cat =>', catSrt.match(regExp),
+    'Bat =>', batSrt.match(regExp),
     'Mat =>', matSrt.match(regExp),
 );
