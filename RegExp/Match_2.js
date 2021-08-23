a8638e1e4f491f412c85917874d5ba65778d03a5
leandrolannister <leandrohendrix@gmail.com>
2021-08-23 19:34:36 -0300

    Regexp Range

--- RegExp/Match_2.js
+++ RegExp/Match_2.js
@@ -1,5 +1,7 @@
+//g->minusculo
+//ig->maiusculo e minusculo
 let nome = "LEANDRO, leandro, little star";
 let regexp = /leandro/ig; 
-let result = nome.match(starRegex) 
+let result = nome.match(regexp); 
 
 console.log(result);
---
+++ RegExp/Range.js
@@ -0,0 +1,5 @@
+let quoteSample = "Ellen 234";
+let regExp = /[a-e2-3]/ig;
+let result = quoteSample.match(regExp);
+
+console.log(result);
