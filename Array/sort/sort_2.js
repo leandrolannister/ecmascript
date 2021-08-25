d8cf2ce877960e1b253ccac95b7eb77044c448a1
leandrolannister <leandrohendrix@gmail.com>
2021-08-25 08:10:09 -0300

    Sort

--- Array/sort/sort_2.js
+++ Array/sort/sort_2.js
@@ -1,13 +1,13 @@
 var lista = [10,1, 5, 9, 8, 12, 15];
 
-function compareNums(a,b){
-   if(a == b) return 0;
+function orderBY(a,b){
+   if(a > b) return 1;
 
    if(a < b) return -1;
 
-   if(a > b) return 1;
+   return 0;
 }
 
-lista.sort(compareNums);
+lista.sort(orderBY);
 
 console.log(lista);
