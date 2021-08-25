aab81b2f82812a9f0dfaa4e8abd37c8417fb3215
leandrolannister <leandrohendrix@gmail.com>
2021-08-25 08:01:11 -0300

    RegExp find a row

--- Array/sort/sort_1.js
+++ Array/sort/sort_1.js
@@ -1,5 +1,5 @@
 //Os valores não são impressos na ordem,
-//pq sort considera a tabela Unicoce para ordenação
+//pq sort considera a tabela Unicode para ordenação
 const lista = [10,1, 5, 9, 8, 12, 15];
 
 lista.sort();
---
+++ RegExp/FindaRow.js
@@ -0,0 +1,5 @@
+//Find letter s in a row. S must be in a sequence
+const name = 'Mississippi';
+let regexp = /s+/ig;
+
+console.log(name.match(regexp));
