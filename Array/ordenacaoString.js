8cf09707221fa279881808374ca854e2b598ce01
leandrolannister <leandrohendrix@gmail.com>
2021-09-24 07:56:26 -0300

    Regexp whitespace

--- Array/ordenacaoString.js
+++ Array/ordenacaoString.js
@@ -1,8 +1,14 @@
-// Ordenar considerando apenas as letras em um array de numeros e letras
- ;
+// Ordenar considerando apenas os números em um array de numeros e letras
+let values = [];
+let regexp = /[\D]/g;
+values.push('x' + 12);
+values.push(10 + 'l');
+values.push(2 + 'f');
+values.push(1 + 'b');
 
-values.sort(function(a,b){
-  return a.replace(/[^0-9]/g, '') - b.replace(/[^0-9]/g, '');
+
+values.sort( (a,b) => {
+  return a.replace(regexp, '') - b.replace(regexp, '');
 });
 
 console.log(values);
---
+++ RegExp/WhiteSpace_2.js
@@ -0,0 +1,5 @@
+//Return number of whitespaces
+let str = 'Leandro Java PHP';
+let regexp = /\s/g;
+
+console.log(str.match(regexp).length);
