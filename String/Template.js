6c7c0e1c86d9d857f922c071f190f7a3dcc71cef
leandrolannister <leandrohendrix@gmail.com>
2021-09-26 08:55:56 -0300

    Template str

---
+++ String/Template.js
@@ -0,0 +1,10 @@
+//Template String
+// ` -> Acento grave
+
+const nome = 'Leandro';
+const idade = 2021 - 1983;
+const cidade = "São Paulo";
+
+
+console.log(`Meu nome ${nome} minha idade ${idade} minha cidade ${cidade}`);
+
