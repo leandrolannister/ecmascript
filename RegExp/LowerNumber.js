dbe7828ff7c9b79da370db1fee71408ae7ec987d
leandrolannister <leandrohendrix@gmail.com>
2021-10-06 07:06:53 -0300

    Add Lower Number RegExp

--- RegExp/LowerNumber.js
+++ RegExp/LowerNumber.js
@@ -1,6 +1,9 @@
-// {1,} -> Indica que a letra a deve se repetir no máximo 4x
+// {4} -> Indica que a letra a deve se repetir no máximo 4x
+// {4,} -> Indica que a letra a deve se repetir mais que máximo 4x
 
-let str = 'Leaaaandro';
-let regexp = /Lea{4,}ndro/g;
+let str = 'Leaaaaandro';
+let max = /Lea{4}ndro/g;
+let moreThan = /Lea{4,}ndro/g;
 
-console.log(str.match(regexp));
+console.log(max.test(str));
+console.log(moreThan.test(str));
---
+++ String/Repeat.js
@@ -0,0 +1,2 @@
+console.log("a".repeat(20));
+
