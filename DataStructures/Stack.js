7c18378e8e707cd423098ff9a440030b796db52d
leandrolannister <leandrohendrix@gmail.com>
2021-10-27 19:06:33 -0300

    Methods peek and size

--- DataStructures/Stack.js
+++ DataStructures/Stack.js
@@ -9,21 +9,32 @@ const Stack = function(){
    }
 
    //Remove
-   this.pop = function(){
+   this.pop = () =>{
       if(this.index === 0){
          return undefined;
       }
 
       this.index--;
       delete this.object[this.index];
-      return this.object[this.index];
-     
+      return this.object[this.index];     
+   }
+
+   this.size = function() {
+      return this.index;
+   }
+
+   this.peek = function(){
+      return this.object[this.index--];
    }
 }
 
 const arr = new Stack();
 arr.add(1);
 arr.add(2);
+arr.add(3);
 arr.pop();
+arr.peek();
 
 console.log(arr.object);
+console.log('Size', arr.size());
+console.log('Peek', arr.peek());
