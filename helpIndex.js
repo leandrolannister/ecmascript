ba9c8a6cffcd8912e5db0d14df9a0c29019282a3
leandrolannister <leandrohendrix@gmail.com>
2021-11-20 17:16:40 -0300

    Every

--- helpIndex.js
+++ helpIndex.js
@@ -1,25 +1,18 @@
-function nome(n) {
+function jaRealizada() {
     return new Promise((resolve, reject) => {
-        if (n == 'leandro')
-            resolve('38');
-        reject('Nome incorrento');
+       return resolve(true);
     });
-}
+  }
+  
+  function nome(){
+    return console.log('Nome: Leandro');
+  }
 
-function cpf(idade) {
-    return new Promise((resolve, reject) => {
-        if (idade == 38)
-            resolve('225609')
-        reject('Idade está incorreta');
-    });
-}
-
-nome('leandro').then((idade) => {
-    cpf(idade).then((cpf) => {
-        console.log(cpf);
-    }).catch((err) => {
-        console.log('Error', err);
-    });
-}).catch((err) => {
-    console.log('Error', err);
-});
+  jaRealizada()
+  .then(nome)
+  .catch((erro) => {
+     console.log('Error', err);
+  })
+  .finally( () => {
+     console.log('Sempre será chamada com ou sem erro');      
+  });
