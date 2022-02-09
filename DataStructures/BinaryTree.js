95d71c2e940c3372e9a903af64ab2f59b8e09e81
hendrix <leandrohendrix@gmail.com>
2022-02-09 07:36:13 -0300

    Binary Tree

--- DataStructures/BinaryTree.js
+++ DataStructures/BinaryTree.js
@@ -1,31 +1,33 @@
 class Node {
-    constructor(val) {
+   constructor(val) {
        this.val = val;
        this.left = null;
        this.right = null;
-    }
- }
- 
- const depthFirstValues = (root) => {
-    const stack = [root];
-    
-    while (stack.length > 0) {
+   }
+}
+
+function builtTree(root) {
+   const stack = [root];
+
+   while (stack.length > 0) {
        let current = stack.pop();
-       
+
        console.log(current.val);
- 
-       if (current.left) stack.push(current.left);
-       if (current.right) stack.push(current.right); 
-    }
- }
- 
- const a = new Node('a');
- a.right  = new Node('b');
- a.left = new Node('c');
- 
- 
- depthFirstValues(a);
- 
- 
- 
- 
+
+       if (current.left){ stack.push(current.left); }
+       if (current.right){ stack.push(current.right); }
+
+   }
+}
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+
+a.left = c;
+a.right = b;
+c.left = d;
+
+
+builtTree(a);
